393a8a6719adf2f4e40ff2f3fc13922e9355d1b4
soumgraphic <soumgraphic@gmail.com>
2017-01-15 11:12:57 +0100

    J'ai mis cela pour faciliter les choses, juste un test lol

--- app/src/main/java/dev/soumaila/com/testandroid/MainActivity.java
+++ app/src/main/java/dev/soumaila/com/testandroid/MainActivity.java
@@ -9,7 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    // la classe principale
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
